8cc83d26a1d5f090dc871b92595dd3acff4365c7
fztrade06-code <fztrade06@gmail.com>
2026-06-10 22:11:03 +0500

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ require('dotenv').config();
 
 const { initDB, getPool } = require('./database');
 
-const app = express();
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
 const PORT = process.env.PORT || 3000;
 app.set('trust proxy', 1);
 
